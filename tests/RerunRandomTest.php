5b79a07ba71e68862e1c7c2f567b1d7326c5a84d
COG_INTERN <karl.klein@cashongo.co.uk>
2015-07-07 16:41:11 +0300

    tests initial &  autoload

---
+++ tests/RerunRandomTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 3.07.15
+ * Time: 15:20
+ *
+ * @author Karl Klein <karl.klein@cashongo.co.uk>
+ */
+
+class RerunTest extends PHPUnit_Framework_TestCase {
+
+    public function random() {
+        $this->assertTrue(rand(0, 1) > 0.5, 'you fail');
+
+    }
+
+
+    public function testOne()
+    {
+        $this->random();
+    }
+
+    public function testTwo()
+    {
+        $this->random();
+    }
+
+    public function testThree()
+    {
+        $this->random();
+    }
+
+    public function testFour()
+    {
+        $this->random();
+    }
+
+    public function testFive()
+    {
+        $this->random();
+    }
+}
