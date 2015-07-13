ee803c414e5edbaff877eb35c796d03830cac6cc
COG_INTERN <karl.klein@cashongo.co.uk>
2015-07-13 17:07:59 +0300

    rerun now includes error, incomplete, risky and skipped tests

--- src/FailureListener.php
+++ src/FailureListener.php
@@ -14,19 +14,45 @@ class PHPUnit_Framework_FailureListener extends PHPUnit_Framework_BaseTestListen
 
     private $failedTests = array();
 
-    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time) {
-        $this->failedTests[] = array(
-            'testMethodName' => $test->getName(),
-            'testClassName'  => get_class($test)
-        );
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        $this->addToFailedTests($test);
     }
 
-    public function endTestSuite(PHPUnit_Framework_TestSuite $suite) {
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->addToFailedTests($test);
+    }
 
+    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->addToFailedTests($test);
+    }
+
+    public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->addToFailedTests($test);
+    }
+
+    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->addToFailedTests($test);
+    }
+
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
 //      if there were failed tests hand off to CacheUtil
         if (! empty($this->failedTests)) {
             $cache = new CacheUtil;
             $cache->createCache($this->failedTests, $_SERVER['argv']);
         }
+    }
+
+    private function addToFailedTests(PHPUnit_Framework_Test $test)
+    {
+        $this->failedTests[] = array(
+            'testMethodName' => $test->getName(),
+            'testClassName'  => get_class($test)
+        );
     }
 }
